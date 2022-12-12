578077bdbb7aecff253edd47706ed6a1c3376d08
Riellytruffa <54604958+Riellytruffa@users.noreply.github.com>
2022-12-12 12:39:32 -0500

    Create madlibslab.py

---
+++ madlibslab.py
@@ -0,0 +1,11 @@
+name = input("Name: ")
+fact1 = input("fact: ")
+fact2 = input("fact: ")
+fave_hobby = input("hobby: ")
+
+
+madlib =f"Greetings! My name is {name}! My favorite food is {fact1}!\
+ My favorite color is {fact2}!\
+ My favorite hobby is {fave_hobby}!"
+
+print(madlib)
